3010d738b43c27867c35aa237c3a21a64962dd17
Ashwani Kumar <ashwanikumar10987@gmail.com>
2022-01-29 18:59:56 +0530

    Salting

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+// const md5 = require("md5");
+const bcrypt=require("bcrypt");
+const saltRounds=10;
 const app = express();
 app.use(express.static("pubic"));
 app.set('view engine','ejs');
@@ -15,14 +17,7 @@ const userSchema= new mongoose.Schema({
   password:String
 });
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
-
 const User=mongoose.model("User",userSchema);
-
-
-
-
-
 
 app.get("/",function(req,res){
   res.render("home");
@@ -31,17 +26,20 @@ app.get("/register",function(req,res){
   res.render("register");
 });
   app.post("/register",function(req,res){
-    const newUser= new User({
-      email:req.body.username,
-      password:req.body.password
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      const newUser= new User({
+        email:req.body.username,
+        password:hash
+      });
+      newUser.save(function(err){
+        if(!err){
+          res.render("secrets");
+        }else{
+          console.log(err);
+        }
+      })
     });
-    newUser.save(function(err){
-      if(!err){
-        res.render("secrets");
-      }else{
-        console.log(err);
-      }
-    })
   });
 app.get("/login",function(req,res){
   res.render("login");
@@ -54,11 +52,13 @@ app.post("/login",function(req,res){
       console.log(err);
     }else{
       if(foundUser){
-        if(foundUser.password===password){
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if(result==true){
           res.render("secrets");
         }
+});
+        }
       }
-    }
   })
 });
 app.listen(3000,function(){
